e4aa0f6faf70de66b9ac03ebf03220198b0b5c6e
Yeisson Osorno <yeisson403@gmail.com>
2019-12-24 17:53:16 -0500

    Service Jwt para crear token OK

---
+++ BackEnd/API/services/jwt.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const jwt = require('jwt-simple');
+const moment = require('moment');
+const secret = 'secret_key_client';
+exports.createToken = function(client){
+    let payload = {
+        sub :client.id,
+        identification : client.identification,
+        name : client.name,
+        lastName : client.lastName,
+        email : client.email,
+        passwordUser = client.passwordUser,        
+        
+        iat : moment().unix(),
+        exp : moment().add(12,'hours').unix
+        
+    
+    }
+    return jwt.encode(payload, secret);
+}
