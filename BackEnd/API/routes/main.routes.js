755321f4aa4d4b8022b2de96895fb516bb4afe67
Yeisson Osorno <yeisson403@gmail.com>
2019-12-25 20:24:54 -0500

    Creación de modelo, control y rutas de los Sensores

--- BackEnd/API/routes/main.routes.js
+++ BackEnd/API/routes/main.routes.js
@@ -5,11 +5,12 @@ const clientController = require('../controllers/clients.control');
 const API = express.Router(); // Load method of express, by name Router,for methods GET and POST
 const md_auth = require('../middleware/authentication');
 
-//Routes test
+//Routes Client
 API.get('/home', clientController.home);
 API.post('/saveClient', clientController.saveClient);
 API.post('/tokenClient', clientController.loginClient);
 API.post('/getClient/:id', md_auth.ensureAuthentication, clientController.getClient);
 API.put('/updateClient/:id', md_auth.ensureAuthentication, clientController.updateClient);
+//Routes Sensors
 
 module.exports = API;
